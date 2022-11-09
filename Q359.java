60d131d15d089183b92001f45c9e66e64088c1ca
Peace Bakare <Peace.Bakare@sterling.ng>
2022-11-09 09:34:18 +0100

    simple logger system framework

---
+++ Q359.java
@@ -0,0 +1,23 @@
+/*
+ * Design a logger system that receives a stream of messages along with their timestamps. 
+ * Each unique message should only be printed at most every 10 seconds 
+ * (i.e. a message printed at timestamp t will prevent other identical messages from being printed until timestamp t + 10).
+ */
+
+
+ //Class name is supposed to be Logger but for the naming paradigm I have chosen.
+public class Q359 {
+    public static void main(String[]args){
+
+    }
+
+    public Q359(){
+
+    }
+
+    public boolean shouldPrintMessage(int timestamp, String Message){
+        Q359 logger = new Q359();
+        
+        return true;
+    }
+}
