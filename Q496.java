42e2b7262bc415ffdc796efe680373bae5316a0e
Peace Bakare <Peace.Bakare@sterling.ng>
2022-11-23 12:00:29 +0100

    Update on the solution

--- Q496.java
+++ Q496.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * 
  * The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
@@ -13,8 +15,39 @@ Return an array ans of length nums1.length such that ans[i] is the next greater 
 
 public class Q496 {
     public int [] nextGreaterElement(int [] num1, int [] num2){
-        
+        int n = num1.length;
+        int [] ans = new int [n];
+        for (int i = 0; i < n; i++){
+            if(!searchNumbers(num1[i], num2)){
+                ans[i] = num1[i];
+            }
+            if(searchNumbers(num1[i], num2)){
+                int j = 0;
+                
+            }
+        }
 
         return new int[] {0, 0, 0};
+    }
+
+    public boolean searchNumbers(int target, int [] numbers){
+        Arrays.sort(numbers);
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+
+            if (target == numbers[mid]){
+                return true;
+            }
+            if (target < numbers[mid]){
+                left = mid + 1;
+            }
+            if (target > numbers[mid]){
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
